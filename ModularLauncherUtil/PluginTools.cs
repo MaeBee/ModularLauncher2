35aa0c8d1ddade4e6a8f1e2abc6425f8e6946cca
gobbo1008 <technofreak91@web.de>
2017-01-30 20:01:26 +0100

    Reference Cleanup
    Cleared out unused but referenced references

--- ModularLauncherUtil/PluginTools.cs
+++ ModularLauncherUtil/PluginTools.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ModularLauncherUtil
+﻿namespace ModularLauncherUtil
 {
     class PluginTools
     {
-        //public static Dictionary<string, string> 
+
     }
 }
